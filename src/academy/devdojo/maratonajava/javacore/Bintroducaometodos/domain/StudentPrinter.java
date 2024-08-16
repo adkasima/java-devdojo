d997af4ca2639502a16d35158c0f921bf2cbaa02
Adriel <adrielkasima@gmail.com>
2024-08-16 10:36:47 -0300

    feat: new classes and concepts

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/domain/StudentPrinter.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain;
+
+public class StudentPrinter {
+    public void print(Student student) {
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.sex);
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/StudentTest01.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.Student;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.domain.StudentPrinter;
+
+public class StudentTest01 {
+    public static void main(String[] args) {
+        Student student01 = new Student();
+        Student student02 = new Student();
+        StudentPrinter printer = new StudentPrinter();
+
+        student01.name = "David";
+        student01.age = 16;
+        student01.sex = 'M';
+
+        student02.name = "Lucy";
+        student02.age = 16;
+        student02.sex = 'F';
+
+        printer.print(student01);
+        System.out.println("----------------");
+        printer.print(student02);
+
+
+//        System.out.println(student01.name);
+//        System.out.println(student01.age);
+//        System.out.println(student01.sex);
+
+
+
+//        System.out.println(student02.name);
+//        System.out.println(student02.age);
+//        System.out.println(student02.sex);
+
+
+
+
+
+
+    }
+}
