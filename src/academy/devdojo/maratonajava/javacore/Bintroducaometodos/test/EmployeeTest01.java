9d3cc3fd210aab1d0469dcf6ee8aae8fff81914b
Adriel <adrielkasima@gmail.com>
2024-08-20 20:22:30 -0300

    refact: get/set methods implemented

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EmployeeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EmployeeTest01.java
@@ -6,9 +6,9 @@ public class EmployeeTest01 {
     public static void main(String[] args) {
         Employee employee = new Employee();
 
-        employee.name = "Jonas";
-        employee.age = 22;
-        employee.wages = new double[]{2.500, 2.600, 2.500};
+        employee.setName("Jonas");
+        employee.setAge(22);
+        employee.setWages(new double[]{2.500, 2.600, 2.500});
 
         employee.print(employee);
 
