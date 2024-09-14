84121e29dd38fd88277f57e16475c693ab9bd4d6
Adriel <adrielkasima@gmail.com>
2024-09-14 17:43:42 -0300

    feat: final modifier (constant)

---
+++ src/academy/devdojo/maratonajava/javacore/Jfinalmodifier/domain/Car.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.Jfinalmodifier.domain;
+
+public class Car {
+    private String name;
+    public static final double LIMIT_SPEED = 250;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Jfinalmodifier/test/CarTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Jfinalmodifier.test;
+
+import academy.devdojo.maratonajava.javacore.Jfinalmodifier.domain.Car;
+
+public class CarTest01 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(Car.LIMIT_SPEED);
+    }
+}
