f83398319d6a4c514e0345a693a3f12b1e62b297
Adriel <adrielkasima@gmail.com>
2025-01-09 11:44:55 -0300

    feat: dates/calendar

---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/CalendarTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest01 {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        if (c.getFirstDayOfWeek() == Calendar.SUNDAY) {
+            System.out.println("Dimingouu!!");
+        }
+        
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+
+        c.add(Calendar.DAY_OF_MONTH, 2);
+        c.roll(Calendar.HOUR, 12);
+        Date date = c.getTime();
+        System.out.println(date);
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1735946629912L);
+        System.out.println(date);
+//        System.out.println(date.getTime());
+    }
+}
